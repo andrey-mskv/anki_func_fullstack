ff17cff4262f7ad2302d6593bcd8256243cd0fd7
Andrei Moskvin <andrey.mskv@gmail.com>
2026-08-08 14:47:16 +0400

    update test.py

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
-from main import load_words
+from main import load_words, save_words
 
 result = load_words('words.txt')
 
 print(result)
+
+words = {
+    'cat': 'кот',
+    'dog': 'собака',
+    'bird': 'птица'
+}
+
+save_words(words, 'test_words.txt')
