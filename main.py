6fff0eecc89acb5d68bf806b2c220372296bcdc5
Andrei Moskvin <andrey.mskv@gmail.com>
2026-08-09 22:46:57 +0400

    feat: add start_game function

--- main.py
+++ main.py
@@ -26,16 +26,73 @@ def load_words(filename):
     return dictionary
 
 
-def print_statistics(score, total_time): ...
+def print_statistics(score, total_time):
+    pass
 
 
-def ask_and_check(word, correct): ...
+def ask_and_check(word, correct):
+    pass
 
 
-def start_game(words): ...
+def start_game(words):
+
+    if not words:
+        print('Словарь пуст. Добавьте слова перед началом игры.')
+        return
+
+    print(
+        'Начинаем игру! Введите перевод слова. Чтобы закончить, введите СТОП'
+    )
+
+    total_time = 0
+    score = 0
+    answer_count = 0
+
+    key_list = list(words.keys())
+
+    while True:
+        random_key = key_list[random.randint(0, len(key_list) - 1)]
+
+        start_time = time.time()
+
+        print(f'Переведите слово: {random_key}')
+
+        user_answer = input('Ваш ответ: ')
+
+        if user_answer.strip().upper() == STOP_WORD:
+            average_time_message = ''
+
+            if answer_count > 0:
+                average_time_message = (
+                    f' (Среднее время ответа:'
+                    f'{total_time / answer_count:.2f} секунд)'
+                )
+
+            print(
+                f'Игра окончена.\nВаш счет: {score}.\n'
+                f'Общее время: {total_time:.2f} секунд{average_time_message}.'
+            )
+
+            break
+
+        answer_time = time.time() - start_time
+        total_time += answer_time
+        answer_count += 1
+
+        if user_answer.strip().lower() == words[random_key].strip().lower():
+            score += 1
+
+            print(f'Верно! Время ответа: {answer_time:.2f} секунд')
+
+        else:
+            print(
+                f'Неверно! Правильный ответ: {words[random_key]}.\n'
+                f'Время ответа: {answer_time:.2f} секунд'
+            )
 
 
-def train_until_mistake(words): ...
+def train_until_mistake(words):
+    pass
 
 
 def add_words(words):
